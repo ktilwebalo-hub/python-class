32e83c1c011e73648558fab9adba9b7db4f33e43
ktilwebalo-hub <ktilwebalo@gmail.com>
2026-01-09 06:35:12 +0000

    git commit --all

---
+++ Loops/divisible_range.py
@@ -0,0 +1,19 @@
+# Write a function `divisible_range(min_val, max_val, num)` that prints all numbers
+# between min_val and max_val (exclusive) that are divisible by num.
+# The function does not return a value, just prints.
+
+def divisible_range(min_val, max_val, num):
+    for i in range(min_val + 1, max_val):
+        if i % num == 0:
+            print(i)
+# Example:
+divisible_range(17, 40, 9)
+# 18
+# 27
+# 36
+
+divisible_range(10, 24, 4)
+# 12
+# 16
+# 20
+
